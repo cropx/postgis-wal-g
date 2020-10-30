915f2dfec9e981e8a5a533ec20e4ad4f46b0d665
damaarten <maarten@crop-r.com>
2020-10-30 12:04:08 +0100

    If no wal-g settings provided, do not enable archiving

--- initdb/initdb-wal-g.sh
+++ initdb/initdb-wal-g.sh
@@ -2,10 +2,15 @@
 if grep -q "wal-g" "/var/lib/postgresql/data/postgresql.conf"; then
     echo "wal-g already configured in /var/lib/postgresql/data/postgresql.conf"
 else
-    echo "adding wal-g archive settings to /var/lib/postgresql/data/postgresql.conf"
-    echo "wal_level = archive" >> /var/lib/postgresql/data/postgresql.conf
-    echo "archive_mode = on" >> /var/lib/postgresql/data/postgresql.conf
-    echo "archive_command = 'wal-g wal-push %p'" >> /var/lib/postgresql/data/postgresql.conf
-    echo "archive_timeout = 60" >> /var/lib/postgresql/data/postgresql.conf
+    # wal-g env settings are of form WALE_* or WALG_*
+    if ! env | -q grep -e '^WALG_' -e '^WALE_'; then
+        echo "No wal-g env variables WALG_* or WALE_* found, WAL file archiving is DISABLED!"
+    else
+        echo "adding wal-g archive settings to /var/lib/postgresql/data/postgresql.conf"
+        echo "wal_level = archive" >> /var/lib/postgresql/data/postgresql.conf
+        echo "archive_mode = on" >> /var/lib/postgresql/data/postgresql.conf
+        echo "archive_command = 'wal-g wal-push %p'" >> /var/lib/postgresql/data/postgresql.conf
+        echo "archive_timeout = 60" >> /var/lib/postgresql/data/postgresql.conf
+    fi
 fi
 
